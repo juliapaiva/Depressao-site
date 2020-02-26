afc0d1444d183ba7d6bc5a39209fe5e84038ce0d
juliapaiva <jupaiva.zelda@gmail.com>
2020-02-26 05:38:31 -0300

    botões adicionados

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 new Vue({
-    
-})
+    el: '#app',
+    data: {
+        playerVida: 100,
+        depreVida: 100,
+        gameRodando: false
+    },
+    methods: {
+        startGame: function() {
+            this.gameRodando = true;
+            this.playerVida = 100;
+            this.depreVida = 100;
+        },
+        terapia: function() {
+
+        },
+        vidasaudavel: function() {
+
+        },
+        amigos: function() {
+
+        },
+        ignorar: function() {
+            
+        }
+
+    }
+});
