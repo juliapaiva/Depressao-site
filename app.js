71870dee6c09e93f38185b1a6335576e277b3ea4
juliapaiva <jupaiva.zelda@gmail.com>
2020-02-27 16:51:14 -0300

    gameataque/ignorar

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ new Vue({
             this.depreVida = 100;
         },
         terapia: function() {
+            max = 12;
+            min = 5;
+            dano = Math.max(Math.floor(Math.random() * max) + 1 , min)
+            this.depreVida -= dano;
+
+            if (this.depreVida <= 0) {
+                alert('Você está no caminho certo! Espero ter te ajudado!')
+                this.gameRodando = false;
+                return;                
+            }
 
         },
         vidasaudavel: function() {
@@ -21,7 +31,18 @@ new Vue({
 
         },
         ignorar: function() {
-            
+            max = 59;
+            min = 5;
+            dano = Math.max(Math.floor(Math.random() * max) + 1 , min)
+            this.playerVida -= dano;
+
+            if (this.playerVida <= 0) {
+                alert('Você não está sozinho(a)! Tudo vai melhorar! Busque ajuda!')
+                this.gameRodando = false;
+                return;                
+            }
+
+        
         }
 
     }
